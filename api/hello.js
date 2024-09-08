009a8226c2c9e4b10219e3cc76af56f249ea3a98
QuangHung <quanghungdang253@gmail.com>
2024-09-08 09:17:12 +0700

    update

--- api/hello.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-    res.status(200).json({
-      title: "Express Testing",
-      message: "The app is working properly!",
-      index: "hello"
-    });
-  };
-  
